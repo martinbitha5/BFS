14c6fbfb4254c3132f37fba3d8b19a76b064f1ea
martinbitha5 <goblaire50@gmail.com>
2026-02-19 13:39:58 +0100

    Fix: Ajouter x-airline-code aux headers autorisés par CORS

--- api/src/server.ts
+++ api/src/server.ts
@@ -68,7 +68,7 @@ const corsOptions = {
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'x-api-key', 'x-airport-code', 'x-user-id', 'x-user-role'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-api-key', 'x-airport-code', 'x-user-id', 'x-user-role', 'x-airline-code'],
   exposedHeaders: ['Content-Type', 'Authorization'],
   optionsSuccessStatus: 200 // Pour les anciens navigateurs
 };
